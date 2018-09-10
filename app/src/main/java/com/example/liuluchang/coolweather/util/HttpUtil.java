2c7f4d0cb572c42b24d6050e310f0f87a176081f
liuluchang <cqllc.123@qq.com>
2018-09-10 15:41:31 +0800

    提交网络工具类

---
+++ app/src/main/java/com/example/liuluchang/coolweather/util/HttpUtil.java
@@ -0,0 +1,23 @@
+package com.example.liuluchang.coolweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Author: liuluchang
+ * Time:  2018/9/10
+ * Description: This is HttpUtil
+ */
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+
+
+    }
+
+}
+
+//    OkHttpClient client = new OkHttpClient();
+//    Request request = new Request.Builder().url(address).build();
+//        client.newCall(request).enqueue(callback);
